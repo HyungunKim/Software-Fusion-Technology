6f5fdd9c2b840f5d59df3ca4106126f5420757f3
HyungunKim <Hyungun.kimm@gmail.com>
2021-11-03 12:52:32 +0900

    torch version

--- lanemod/lanelib.py
+++ lanemod/lanelib.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import PIL.Image as Image
 import random
 from tqdm import tqdm_notebook
-import tensorflow as tf
+import torch
 from sklearn.cluster import DBSCAN
 
 def preX(names, dataFile='data'):
@@ -12,14 +12,16 @@ def preX(names, dataFile='data'):
     for i, name in enumerate(names):
         img = Image.open(f'{dataFile}/TrainData/Train/{name}')
         X[i] = np.array(img)[:,:,:3]/255
+    X = np.transpose(X, (0, 3, 1, 2))
+    X = torch.tensor(X, dtype=torch.float, requires_grad=True)
     return X
 
 def preY(names, dataFile='data'):
     Y = np.zeros((len(names), 256, 512))
     for i, name in enumerate(names):
         img = Image.open(f'{dataFile}/MaskedData/Masked/{name}')
-        y = np.array(img)/255
-        Y[i] = y
+        Y[i] = np.array(img)/255
+    Y = torch.tensor(Y, dtype=torch.float, requires_grad=True)
     return Y
     
 def getNames(names, n, preshuffle=True):
@@ -29,15 +31,90 @@ def getNames(names, n, preshuffle=True):
     else:
         return random.shuffle(names[:n]), names[n:]
     
-    
 def WBCE(y_true, y_pred):
-    epsilon = 1e-7
-    return (-1)*tf.math.reduce_mean(tf.math.multiply(tf.math.log(y_pred + epsilon), y_true)*20 + tf.math.multiply(tf.math.log(1-y_pred + epsilon), 1-y_true))
+    epsilon = torch.tensor(1e-7).to(DEVICE)
+    return ((-1)*torch.mean(torch.multiply(torch.log(y_pred + epsilon), y_true)*20 + torch.multiply(torch.log(1-y_pred + epsilon), 1-y_true))).to(DEVICE)
+
+@torch.no_grad()
+def show_pred_image(X, Y_pred, Y):
+    X = np.transpose(X.cpu().detach().numpy(), (1,2,0))
+    plt.imshow(X, cmap='gray')
+    plt.show()
     
+    Y_pred = np.squeeze(Y_pred.cpu().detach().numpy())    
+    plt.imshow(Y_pred, cmap= 'gray')
+    plt.show()
+
+    Y = np.squeeze(Y.cpu().detach().numpy())
+    plt.imshow(Y, cmap = 'gray')
+    plt.show()
+
     
+def Train(model, img_names, valid_names=None, epochs=1, chunk_size=500, batch_size=5):
+    num_chunks = len(img_names)//chunk_size
+    if len(img_names)%chunk_size:
+        num_chunks += 1
     
+    total_batch_num = len(img_names)//batch_size + len(img_names)%batch_size
+
+    if valid_names is not None:
+        num_chunks_val = len(valid_names)//chunk_size
+        if len(valid_names)%chunk_size:
+            num_chunks_val += 1
     
-    
+    model.train(True)
+    for e in range(epochs):
+        print(f'##### Epoch {e}/{epochs} Train Start #####')
+        for chunk in notebook.tqdm(range(num_chunks)):
+            names = img_names[chunk_size*chunk: chunk_size*(chunk+1)]
+            X_chunk = preX(names)
+            Y_chunk = preY(names)
+
+            num_batchs = chunk_size//batch_size
+            if chunk_size%batch_size:
+                num_batchs += 1
+            
+            for batch in notebook.tqdm(range(num_batchs)):
+                X = X_chunk[batch_size*batch: batch_size*(batch + 1)].to(DEVICE)
+                Y = Y_chunk[batch_size*batch: batch_size*(batch + 1)].to(DEVICE)
+
+                optimizer.zero_grad()
+                Y_pred = model(X)
+                loss = WBCE(Y, Y_pred)
+                loss.backward()
+                optimizer.step()
+                print('\r', f'[Train] Current Epoch : {e + 1}/{epochs}, Current Batch : {chunk*num_batchs+batch+1}/{total_batch_num}, WBCE : {loss.cpu()}', end = '')
+                if (chunk*num_batchs+batch+1) % (total_batch_num//20) == 0:
+                    print()
+                
+                break
+            break
+
+        # Validation
+        with torch.no_grad():
+            if valid_names is not None:
+                print(f"##### Epoch {e}/{epochs} Validation Start #####")
+                val = []
+                for chunk_val in notebook.tqdm(range(num_chunks_val)):
+                    names = valid_names[batch_size*chunk_val: batch_size*(chunk_val + 1)]
+                    X_chunk = preX(names)
+                    Y_chunk = preY(names)
+
+                    num_batchs = chunk_size//batch_size
+                    if chunk_size%batch_size:
+                        num_batchs += 1
+
+                    for batch in notebook.tqdm(range(num_batchs)):
+                        X = X_chunk[batch_size*batch: batch_size*(batch + 1)].to(DEVICE)
+                        Y = Y_chunk[batch_size*batch: batch_size*(batch + 1)].to(DEVICE)
+                    
+                        Y_pred = model(X)
+                        loss = WBCE(Y, Y_pred)
+                        val.append(loss.item())
+                        
+                    
+                print(f'[Validation] Current Epoch : {e}/{epochs}, WBCE : {sum(val)/len(val)}')
+                show_pred_image(X[0],Y_pred[0], Y[0])
     
     
     
--- lanemod/networks.py
+++ lanemod/networks.py
@@ -1,198 +1,127 @@
-import tensorflow as tf
-from tensorflow import keras 
-from tensorflow.keras import Input, Model
-from tensorflow.keras.layers import Conv2D, MaxPool2D, Dense, BatchNormalization, UpSampling2D
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
 
-class Encoder(keras.layers.Layer):
+class Encoder(nn.Module):
     def __init__(self, inch, outch):
         super().__init__()
-        self.conv1 = Conv2D((inch + outch)//2, 3, padding='same')
-        self.bn1 = BatchNormalization()
+        midch = (inch + outch)//2
+        self.conv1 = nn.Conv2d(inch, midch, kernel_size = (3, 3), padding='same')
+        self.bn1 = nn.BatchNorm2d(midch)
 
-        self.conv2 = Conv2D(outch, 3, padding='same')
-        self.bn2 = BatchNormalization()
+        self.conv2 = nn.Conv2d(midch, outch, kernel_size = (3, 3), padding='same')
+        self.bn2 = nn.BatchNorm2d(outch)
 
-        self.mp = MaxPool2D(pool_size=(2,2), strides=(2,2), padding='same')
+        self.mp = nn.MaxPool2d(kernel_size=(2,2), stride = (2,2))
+    
+        self.conv3 = nn.Conv2d(outch, outch, kernel_size = (3, 3), padding='same')
+        self.bn3 = nn.BatchNorm2d(outch)
 
-        self.conv3 = Conv2D(outch, 3, padding='same')
-        self.bn3 = BatchNormalization()
-
-    def call(self, input_tensor, training=True, *args, **kwargs):
+    def forward(self, input_tensor, *args, **kwargs):
         x = self.conv1(input_tensor)
-        x = self.bn1(x, training=training)
-        x = tf.nn.relu(x)
+        x = self.bn1(x)
+        x = F.relu(x)
 
         x = self.conv2(x)
-        x = self.bn2(x, training=training)
-        x = tf.nn.relu(x)
+        x = self.bn2(x)
+        x = F.relu(x)
 
         x = self.mp(x)
 
         x = self.conv3(x)
-        x = self.bn3(x, training=training)
-        x = tf.nn.relu(x)
+        x = self.bn3(x)
+        x = F.relu(x)
         return x
 
-        
-class Decoder(keras.layers.Layer):
+class Decoder(nn.Module):
     def __init__(self, inch, outch):
         super().__init__()
-        self.conv1 = Conv2D((inch + outch)//2, 3, padding='same')
-        self.bn1 = BatchNormalization()
+        midch = (inch + outch)//2
 
-        self.conv2 = Conv2D(outch, 3, padding='same')
-        self.bn2 = BatchNormalization()
+        self.conv1 = nn.Conv2d(inch, midch, kernel_size = (3, 3), padding='same')
+        self.bn1 = nn.BatchNorm2d(midch)
 
-        self.up = UpSampling2D(size=(2,2))
+        self.conv2 = nn.Conv2d(midch, outch, kernel_size = (3, 3), padding='same')
+        self.bn2 = nn.BatchNorm2d(outch)
+        
+        self.up = nn.Upsample(scale_factor=2)
 
-        self.conv3 = Conv2D(outch, 3, padding='same')
-        self.bn3 = BatchNormalization()
+        self.conv3 = nn.Conv2d(outch, outch, kernel_size = (3, 3), padding='same')
+        self.bn3 = nn.BatchNorm2d(outch)
 
-    def call(self, input_tensor, *args, training=True, **kwargs):
+    def forward(self, input_tensor, *args, **kwargs):
         x = self.conv1(input_tensor)
-        x = self.bn1(x, training=training)
-        x = tf.nn.relu(x)
+        x = self.bn1(x)
+        x = F.relu(x)
 
         x = self.conv2(x)
-        x = self.bn2(x, training=training)
-        x = tf.nn.relu(x)
+        x = self.bn2(x)
+        x = F.relu(x)
 
         x = self.up(x)
 
         x = self.conv3(x)
-        x = self.bn3(x, training=training)
-        x = tf.nn.relu(x)
-        
+        x = self.bn3(x)
+        x = F.relu(x)
         return x
 
-class Residual(keras.layers.Layer):
+class Residual(nn.Module):
     def __init__(self, inch, hich):
         super().__init__()
-        self.conv1 = Conv2D(hich, 3, padding='same')
-        self.bn1 = BatchNormalization()
+        self.conv1 = nn.Conv2d(inch, hich, kernel_size = (3, 3), padding='same')
+        self.bn1 = nn.BatchNorm2d(hich)
 
-        self.conv2 = Conv2D(inch, 3, padding='same')
-        self.bn2 = BatchNormalization()
+        self.conv2 = nn.Conv2d(hich, inch, kernel_size = (3, 3), padding='same')
+        self.bn2 = nn.BatchNorm2d(inch)
 
-    def call(self, input_tensor, *args, training=True, **kwargs):
+    def forward(self, input_tensor, *args, **kwargs):
         x = self.conv1(input_tensor)
-        x = self.bn1(x, training=training)
-        x = tf.nn.relu(x)
+        x = self.bn1(x)
+        x = F.relu(x)
 
         x = self.conv2(x)
-        x = self.bn2(x, training=training)
-        x += input_tensor
-        return tf.nn.relu(x)
+        x = self.bn2(x)
+        x = F.relu(x + input_tensor)
+        return x
 
-class Aggregation(keras.layers.Layer):
+class Aggregation(nn.Module):
     def __init__(self, inch, outch, hich=None):
         if hich is None:
             hich = outch
         super().__init__()
-        self.conv1 = Conv2D(hich, 3, padding='same')
-        self.bn1 = BatchNormalization()
-        self.up = UpSampling2D(size=(2,2))
-        self.conv2 = Conv2D(outch, 3, padding='same')
-        self.bn2 = BatchNormalization()
-
-    def call(self, input_tensor, other_tensor, *args, training=True, **kwargs):
-        x = self.conv1(input_tensor)
-        x = self.bn1(x, training=training)
-        x = tf.nn.relu(x)
-        x = self.conv2(x+self.up(other_tensor))
-        x = self.bn2(x, training=training)
-        # x += input_tensor
-        return tf.nn.relu(x)
-
-class LaneNetBig(keras.Model):
-    def __init__(self):
-        super().__init__()
-        self.Encoder1 = Encoder(3, 32)        # input channel : 3,  output channel : 32
-        self.Encoder2 = Encoder(32, 64)       # input channel : 32, output channel : 64
-        self.Encoder3 = Encoder(64, 128)      # input channel : 64,  output channel : 128
-        self.Residual1 = Residual(128, 256)   # input channel : 128  output channel : 256
-        self.Residual2 = Residual(128, 256)   # input channel : 128,  output channel : 256
-        self.Decoder1 = Decoder(128, 64)      # input channel : 128,  output channel : 64
-        self.Decoder2 = Decoder(64, 32)       # input channel : 64,  output channel : 32
-        self.Decoder3 = Decoder(32, 1)        # input channel : 32,  output channel : 1
-
-        self.Aggregation1 = Aggregation(3, 32)
-        self.Aggregation2 = Aggregation(32, 64)
-        self.Aggregation3 = Aggregation(64, 128)
-        self.Aggregation4 = Aggregation(128, 128)
-        self.Aggregation5 = Aggregation(128, 64)
-        self.Aggregation6 = Aggregation(64, 32)
-        self.Aggregation7 = Aggregation(32, 1, hich=32)
+        self.conv1 = nn.Conv2d(inch, hich, kernel_size = (3, 3), padding='same')
+        self.bn1 = nn.BatchNorm2d(hich)
         
-        self.AggregationA = Aggregation(32, 64)
-        self.AggregationB = Aggregation(64, 128)
-        self.AggregationC = Aggregation(128, 128)
-        self.AggregationD = Aggregation(128, 64)
-        self.AggregationE = Aggregation(64, 32, hich=64)
+        self.up = nn.Upsample(scale_factor=2)
 
-        self.Aggregation_a = Aggregation(64, 128)
-        self.Aggregation_b = Aggregation(128, 128)
-        self.Aggregation_c = Aggregation(128, 64, hich = 128)
+        self.conv2 = nn.Conv2d(hich, outch, kernel_size = (3, 3), padding='same')
+        self.bn2 = nn.BatchNorm2d(outch)
 
+    def forward(self, input_tensor, other_tensor, *args, **kwargs):
+        x = self.conv1(input_tensor)
+        x = self.bn1(x)
+        x = F.relu(x)
 
-    def call(self, input_tensor, *args, training=True, **kwargs):
-        e1o = self.Encoder1(input_tensor, training=training) 
+        x = self.conv2(x+self.up(other_tensor))
+        x = self.bn2(x)
+        x = F.relu(x)
 
-        e2o = self.Encoder2(e1o, training=training)
+        return x
 
-        e3o = self.Encoder3(e2o, training=training) 
-
-        r1o = self.Residual1(e3o, training=training) 
-
-        r2o = self.Residual2(r1o, training=training) 
-
-        d1o = self.Decoder1(r2o, training=training) 
-
-        d2o = self.Decoder2(d1o, training=training) 
-
-        d3o = self.Decoder3(d2o, training=training) 
-
-        a1o = self.Aggregation1(input_tensor, e1o)
-
-        b1o = self.AggregationA(e1o, e2o, training=training)
-
-        a2o = self.Aggregation2(a1o, b1o, training=training)
-
-        c1o = self.Aggregation_a(e2o, e3o, training=training)
-
-        b2o = self.AggregationB(b1o, c1o, training=training)
-
-        a3o = self.Aggregation3(a2o, b2o, training=training)
-
-        c2o = self.Aggregation_b(c1o, r1o, training=training)
-
-        b3o = self.AggregationC(b2o, c2o, training=training)
-
-        a4o = self.Aggregation4(a3o, b3o, training=training)
-
-        c3o = self.Aggregation_c(c2o, r2o, training=training)
-
-        b4o = self.AggregationD(b3o, c3o, training=training)
-
-        a5o = self.Aggregation5(a4o, b4o, training=training)
-
-        b5o = self.AggregationE(b4o, d1o, training=training)
-
-        a6o = self.Aggregation6(a5o, b5o, training=training)
-
-        a7o = self.Aggregation7(a6o, d2o, training=training)
-
-        return tf.math.sigmoid(d3o + a7o)
-
-class LaneNet(keras.Model):
-    def __init__(self):
+class LaneNet(nn.Module):
+    def __init__(self, DEVICE):
         super().__init__()
         self.e1 = Encoder(3, 32)
         self.e2 = Encoder(32, 64)
+        
         self.r1 = Residual(64, 128)
         self.r2 = Residual(64, 128)
+
         self.d1 = Decoder(64, 32)
         self.d2 = Decoder(32, 1)
+
         self.a1 = Aggregation(3, 32)
         self.a2 = Aggregation(32, 64)
         self.a3 = Aggregation(64, 64)
@@ -202,27 +131,25 @@ class LaneNet(keras.Model):
         self.b1 = Aggregation(32, 64)
         self.b2 = Aggregation(64, 64)
         self.b3 = Aggregation(64, 32, hich=64)
-
-
-    def call(self, input_tensor, *args, training=True, **kwargs):
-        e1o = self.e1(input_tensor, training=training) 
-        e2o = self.e2(e1o, training=training) 
-        r1o = self.r1(e2o, training=training) 
-        r2o = self.r2(r1o, training=training) 
-        d1o = self.d1(r2o, training=training) 
-        d2o = self.d2(d1o, training=training) 
+        
+        self.DEVICE = DEVICE
+        self.to(DEVICE)
+        
+    def forward(self, input_tensor, *args, **kwargs):
+        e1o = self.e1(input_tensor) 
+        e2o = self.e2(e1o)
+        r1o = self.r1(e2o)
+        r2o = self.r2(r1o)
+        d1o = self.d1(r2o)
+        d2o = self.d2(d1o)
 
         a1o = self.a1(input_tensor, e1o)
+        b1o = self.b1(e1o, e2o)
+        a2o = self.a2(a1o, b1o)
+        b2o = self.b2(b1o, r1o)
+        a3o = self.a3(a2o, b2o)
+        b3o = self.b3(b2o, r2o)
+        a4o = self.a4(a3o, b3o)
+        a5o = self.a5(a4o, d1o)
 
-        b1o = self.b1(e1o, e2o, training=training)
-        a2o = self.a2(a1o, b1o, training=training)
-
-        b2o = self.b2(b1o, r1o, training=training)
-        a3o = self.a3(a2o, b2o, training=training)
-
-        b3o = self.b3(b2o, r2o, training=training)
-        a4o = self.a4(a3o, b3o, training=training)
-
-        a5o = self.a5(a4o, d1o, training=training)
-
-        return tf.math.sigmoid(d2o + a5o)
+        return torch.squeeze(torch.sigmoid(d2o + a5o))
